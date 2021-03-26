27beb059fb7374552c1dd150a4b624d17d70d7be
marco <marcotulio21mm@hotmail.com>
2021-03-26 11:43:45 -0300

    incio

---
+++ class/Sql.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Project;
+
+class Sql{
+
+    private $conn;
+
+    public function __construct(){
+
+        $conn= new PDO("mysql: host= localhost; dbname=revisão", "root", "");
+
+    }
+
+    private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+    public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+    public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+    
+}
+
+?>
